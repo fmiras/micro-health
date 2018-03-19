14f82e8a53cea80c8f6a3fa7af7eb0ee68f18c72
Federico Miras <miras.federico@gmail.com>
2018-03-19 13:26:45 -0300

    Support custom error checker

--- test/index.js
+++ test/index.js
@@ -1,5 +1,8 @@
 const test = require('ava')
-const health = require('../lib')
+const sinon = require('sinon')
+const rewire = require('rewire')
+
+const health = rewire('../lib')
 
 test('Return success on health check', t => {
   const microFn = () => 'Micro Response'
@@ -15,4 +18,39 @@ test('Execute microservice if isn\'t health check', t => {
 
   const req = { url: '/' }
   t.is('Micro Response', handler(req))
+})
+
+test('With custom checker if !error then success', t => {
+  const microFn = () => 'Micro Response'
+  const errorChecker = sinon.stub()
+  errorChecker.callsFake(() => false)
+  const handler = health(microFn, errorChecker)
+
+  const req = { url: '/health' }
+  handler(req)
+  t.true(errorChecker.calledOnce)
+})
+
+test('With custom checker if !error send 500', t => {
+  const microFn = () => 'Micro Respo  nse'
+  health.__set__('send', sinon.stub().callsFake(() => {
+    throw new Error('Error!')
+  }))
+  const errorChecker = sinon.stub().returns({ error: 'big error' })
+  const handler = health(microFn, errorChecker)
+
+  const req = { url: '/health' }
+  t.throws(() => handler(req))
+})
+
+test('With custom checker that isn\'t a function', t => {
+  const microFn = () => 'Micro Respo  nse'
+  health.__set__('send', sinon.stub().callsFake(() => {
+    throw new Error('Error!')
+  }))
+  const errorChecker = { foo: 'bar' }
+  const handler = health(microFn, errorChecker)
+
+  const req = { url: '/health' }
+  t.throws(() => handler(req))
 })
